f5a4783b3b2866f00f61bc27931396c6b0197526
youngsik <doly6@naver.com>
2025-06-05 16:54:23 +0900

    Update DeviceDriver.java

--- src/main/java/DeviceDriver.java
+++ src/main/java/DeviceDriver.java
@@ -3,16 +3,35 @@
  */
 public class DeviceDriver {
 
+    FlashMemoryDevice hw;
     public DeviceDriver(FlashMemoryDevice hardware) {
         // TODO: implement this method
+        hw = hardware;
     }
 
     public byte read(long address) {
         // TODO: implement this method
-        return -1;
+        byte originData =hw.read(address) ;
+
+        for(int i=0; i<4; i++)
+        {
+            byte testdata=hw.read(address);
+            if(testdata!=originData)
+            {
+                throw new ReadFailException("error");
+            }
+        }
+
+        return originData;
     }
 
     public void write(long address, byte data) {
         // TODO: implement this method
+        byte curdata=hw.read(address);
+        if(curdata !=(byte) 0xFF)
+        {
+            throw new WriteFailException("Address already has data");
+        }
+        hw.write(address,data);
     }
 }
